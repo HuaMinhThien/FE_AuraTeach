7383396b642e8a57952f1286c081b32cb841bcb2
ndaminh26062006-coder <ndaminh26062006@gmail.com>
2026-08-07 18:42:30 +0700

    be

--- fe_aurateach/src/services/apiClient.js
+++ fe_aurateach/src/services/apiClient.js
@@ -1,4 +1,4 @@
-const API_BASE_URL =  "http://localhost:8000/api";
+const API_BASE_URL =  "http://api.aurateach.io.vn/api";
 
 const getHeaders = (customHeaders = {}, isFormData = false) => {
   // 🔍 Hỗ trợ tìm token ở nhiều key phổ biến khác nhau trong localStorage
